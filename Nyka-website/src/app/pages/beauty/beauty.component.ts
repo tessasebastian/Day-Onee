61a2eb76bdc2867b879bf015662ebaeab8734d9c
tessasebastian <149649528+tessasebastian@users.noreply.github.com>
2024-08-04 22:23:39 +0530

    Add files via upload

---
+++ Nyka-website/src/app/pages/beauty/beauty.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beauty',
+  standalone: true,
+  imports: [],
+  templateUrl: './beauty.component.html',
+  styleUrl: './beauty.component.css'
+})
+export class BeautyComponent {
+
+}
