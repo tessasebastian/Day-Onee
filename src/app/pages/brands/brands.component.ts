3e6fd5c44c94a6c99f5982b3311d8aabad41b855
tessasebastian <149649528+tessasebastian@users.noreply.github.com>
2024-08-07 20:19:08 +0530

    Add files via upload

---
+++ src/app/pages/brands/brands.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-brands',
+  standalone: true,
+  imports: [],
+  templateUrl: './brands.component.html',
+  styleUrl: './brands.component.css'
+})
+export class BrandsComponent {
+
+}
